c4451994a52195833f61473f1f0d13ce7df1d672
Morgan Matney <morgan.matney89@gmail.com>
2020-09-07 18:09:57 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,6 +9,6 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+    backer.backed_projects << self
   end
 end
